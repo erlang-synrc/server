2fda58ac48f4b3146b3ad2208b565a21eee8ea63
Alexandr Kalenuk <akalenuk@gmail.com>
2012-09-13 12:12:07 +0300

    Start with gifts web part

--- apps/nsm_srv/include/common.hrl
+++ apps/nsm_srv/include/common.hrl
@@ -1,1 +1,51 @@
 -define(ELSE, true).
+
+-record(ext_product_info,
+        {
+         vendor_id,
+         id,
+         active,
+         name,
+         category,
+         category_name,
+         short_descr,
+         long_descr,
+         small_image_url,
+         big_image_url,
+         in_stock,
+         retailer_price,
+         user_price
+        }).
+
+-record(gift,
+        {
+         id                     :: integer(), % auto
+         vendor_id              :: integer(), % auto
+         categories             :: list(integer()), % admin
+         ext_gift_id            :: term(),    % ext
+         gift_name              :: binary(),  % admin
+         ext_gift_name          :: binary(),  % ext
+         description_short      :: binary(),  % admin (based on ext)
+         description_long       :: binary(),  % admin (based on ext)
+         image_small_url        :: binary(),  % admin (based on ext)
+         image_big_url          :: binary(),  % admin (based on ext)
+         publish_start_date     :: calendar:date_time(), % admin
+         publish_end_date       :: calendar:date_time(), % admin
+         real_price             :: integer(), % ext
+         retailer_price         :: integer(), % ext
+         in_stock,            % FIXME: WTF? ext
+         enabled_on_site        :: boolean(), % admin
+         kakush_point           :: integer(), % admin
+         kakush_currency,     % FIXME: WTF? admin
+         creation_date          :: calendar:date_time(), % auto
+         modify_date            :: calendar:date_time()  % auto
+        }).
+
+-record(gifts_category,
+        {
+         id             :: integer(),
+         name           :: binary(),
+         description    :: binary(),
+         parent         :: undefined | integer()
+        }).
+
--- apps/nsw_srv/src/gifts.erl
+++ apps/nsw_srv/src/gifts.erl
@@ -3,29 +3,79 @@
 -compile(export_all).
 
 -include_lib("nitrogen_core/include/wf.hrl").
+-include_lib("nsm_srv/include/common.hrl").
+-include("setup.hrl").
+-include("common.hrl").
 
--include("gettext.hrl").
 
 main() ->
-    webutils:add_script("/nitrogen/gifts.js"),
+%    webutils:add_script("/nitrogen/gifts.js"),
     webutils:add_script("/nitrogen/blockui.js"),    %PUBLIC BETA this is for blocking undone content
-    webutils:add_raw("
-        <script type=\"text/javascript\">
-        $(document).ready(function(){
-            $('div.for_blocking').block({ 
-                message: '<h1>" ++ ?_T("This feature is not yet available in beta.") ++ "</h1><h2>" ++ ?_T("You will see this part very soon.") ++ "</h2>', 
-                css: { border: '3px solid #a00' } 
-            });
-        });
-        </script>
-    "),
+%    webutils:add_raw("
+%        <script type=\"text/javascript\">
+%        $(document).ready(function(){
+%            $('div.for_blocking').block({ 
+%                message: '<h1>" ++ ?_T("This feature is not yet available in beta.") ++ "</h1><h2>" ++ ?_T("You will see this part very soon.") ++ "</h2>', 
+%                css: { border: '3px solid #a00' } 
+%            });
+%        });
+%        </script>
+%    "),
 
     #template { file=code:priv_dir(nsw_srv)++"/templates/bare.html" }.
+
+ensafe(In) ->
+    get_file:replace(
+    get_file:replace(
+
+    get_file:replace(
+    get_file:replace(
+    get_file:replace(
+    get_file:replace(
+
+    get_file:replace(
+    get_file:replace(
+    get_file:replace(
+    get_file:replace(In,[304], "İ"),
+                        [350], "Ş"),
+                        [253], "ı"),
+                        [221], "İ"),
+
+                        [254], "ş"),
+                        [222], "Ş"),
+                        [240], "ğ"),
+                        [208], "Ğ"),
+
+                        [231], "ç"),
+                        [199], "Ç").
+
 
 title() -> webutils:title(?MODULE).
 
 body() ->
-    #template{file=code:priv_dir(nsw_srv)++"/templates/inner_page3.html"}.
+%    #template{file=code:priv_dir(nsw_srv)++"/templates/inner_page3.html"}.
+    AllGiftsData = rpc:call(?APPSERVER_NODE, nsm_gifts_db, get_all_gifts, []),
+    OnlyGiftsData = lists:sublist( [Gift || {Gift, _Obj} <- AllGiftsData], 288),
+%    ?INFO("Gifts: ~p", [[OneGift#gift.gift_name || OneGift <- OnlyGiftsData]]),
+    [   "<section id='main'>
+			<div class='top-space top-space-2'>
+				<h1>Hedİyeler</h1>
+			</div>",
+        [
+            begin
+                case string:str(ensafe(OneGift#gift.gift_name), "ikolata Kap") of
+                    0 -> ok;
+                    _ -> ?INFO("Letter is: ~p", [hd(OneGift#gift.gift_name)])
+                end,
+                [
+                    #label{text=ensafe(OneGift#gift.gift_name)},
+                    #br{}
+                ]
+            end
+            || OneGift <- OnlyGiftsData
+        ],
+        "</section>"
+    ].
 
 html_tooltip() ->
     ["<div class=\"tooltip-2\" style=\"display:none\">
