7073c7f6ed9fb21be8eb52dc99b40b17a8448af4
Maxim Sokhatsky <maxim@synrc.com>
2012-09-14 15:01:01 +0300

    enable embedde riak and rabbit client for any type of node

--- apps/nsm_db/src/nsm_db_app.erl
+++ apps/nsm_db/src/nsm_db_app.erl
@@ -19,6 +19,7 @@ wait_vnodes() ->
 start(_StartType, _StartArgs) ->
     application:start(nsx_utils),
     application:start(nsx_config),
+    application:start(nsm_mq),
     nsm_db:start(),
     nsm_db:initialize(),
     ?INFO("Waiting for Riak to Initialize...."),
