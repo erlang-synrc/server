507eb105b5f53d4a9a358b2e2535b46b6de8a6c0
Maxim Sokhatsky <maxim@synrc.com>
2012-09-17 14:39:51 +0300

    another fix :(

--- apps/nsw_srv/src/nsw_srv_sup.erl
+++ apps/nsw_srv/src/nsw_srv_sup.erl
@@ -97,6 +97,8 @@ init([]) ->
          _ -> create_tables(100)
     end,
 
+    rpc:call(?APPSERVER_NODE,nsm_bg,init_workers,[]),
+
     Dispatch = [{'_', [ {'_',nitrogen_cowboy,[]},
                         {['...'],cowboy_http_static,[{directory,{priv_dir,nsw_srv,[]},{mimetypes,mime()}}]} ] }], 
     HttpOpts = [{max_keepalive, 50}, {dispatch, Dispatch}],
