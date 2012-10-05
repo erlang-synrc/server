45e5b90035fe7976590f11175b968798fb61a9d0
Maxim Sokhatsky <maxim@synrc.com>
2012-10-05 16:42:55 +0300

    config files for production

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+NODE=`hostname -a`
+
+cp prod/$NODE/app.config rels/app/node/etc/app.config
+cp prod/$NODE/game.config rels/game/node/etc/app.config
+cp prod/$NODE/web.config rels/web/node/etc/app.config
+
+cp prod/$NODE/app.vm.args rels/app/node/etc/vm.args
+cp prod/$NODE/game.vm.args rels/game/node/etc/vm.args
+cp prod/$NODE/web.vm.args rels/web/node/etc/vm.args
