7a5a4979314b6b6dc271e4aec29ef5a6c81355f4
Maxim Sokhatsky <maxim@synrc.com>
2012-10-05 13:07:58 +0300

    8000 back

--- configure.sh
+++ configure.sh
@@ -8,6 +8,6 @@ cd rels/app/node/etc
 cd ../../../game/node/etc
 ./configure -app $APP -game game -game-port 9000 -game-host $LOCAL_IP -web web -mnesia-init -mq-user guest -mq-pass guest
 cd ../../../web/node/etc
-./configure -ip $LOCAL_IP -app $APP -web web -web-port 80 -game game -srv $LOCAL_IP -srv-host $LOCAL_IP -srv-port 9000 -mq-user guest -mq-pass guest -fb-app-id 274618369298354 -fb-app-secret c7824599489d4dd24eee251c20174959 -jspack full -csspack full
+./configure -ip $LOCAL_IP -app $APP -web web -web-port 8000 -game game -srv $LOCAL_IP -srv-host $LOCAL_IP -srv-port 9000 -mq-user guest -mq-pass guest -fb-app-id 274618369298354 -fb-app-secret c7824599489d4dd24eee251c20174959 -jspack full -csspack full
 cd ../../../..
 
