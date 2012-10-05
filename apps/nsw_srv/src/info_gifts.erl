71ff6d2350cc9be26424194a5935a791793aa5fb
Alexandr Kalenuk <akalenuk@gmail.com>
2012-10-05 12:00:36 +0300

    English info pages

--- apps/nsw_srv/src/info_gifts.erl
+++ apps/nsw_srv/src/info_gifts.erl
@@ -18,6 +18,12 @@ menu() ->
     Request = wf_context:request_bridge(),
     BasePath = Request:path(),
     En = uri_translator:translate(BasePath),
+    case site_utils:detect_language() of
+        "en" -> menu_en(En);
+        "tr" -> menu_tr(En)
+    end.
+
+menu_tr(En) ->
     [
         case En of 
            "/info-gifts" ->
@@ -61,9 +67,62 @@ menu() ->
         #link{text=?_T("ÜYE OL!"), url=?_U("/login/register"), class="info-page-menu-elements info-page-join-link"}
     ].
 
-content() ->    % PUBLIC BETA will have to redo this with css    
+menu_en(En) ->
+    [
+        case En of 
+           "/info-gifts" ->
+                #image{image="/images/more_info/top_plask_1.png", style="margin-top:-15px; margin-left:-24px;"};
+            "/info-tournaments" ->
+                #image{image="/images/more_info/top_plask_2.png", style="margin-top:-15px; margin-left:-24px;"};
+            "/info-social" ->
+                #image{image="/images/more_info/top_plask_3.png", style="margin-top:-15px; margin-left:-24px;"}; 
+            "/info-matchmaker" ->
+                #image{image="/images/more_info/top_plask_4.png", style="margin-top:-15px; margin-left:-24px;"};  
+             "/info-why" ->
+                #image{image="/images/more_info/top_plask_5.png", style="margin-top:-15px; margin-left:-24px;"};   
+            _ ->
+                #image{image="/images/more_info/top_plask.png", style="margin-top:-15px; margin-left:-24px;"}
+        end,
+        case En of
+           "/info-gifts" ->
+                 #label{text=?_T("GIFTS"), class="info-page-menu-elements info-page-gifts-label"};
+           _ ->  #link{text=?_T("GIFTS"), url=?_U("/info-gifts"), class="info-page-menu-elements info-page-gifts-link"}
+        end,
+        case En of 
+             "/info-tournaments" ->
+                  #label{text=?_T("TOURNAMENTS"), class="info-page-menu-elements info-page-tournaments-label", style="letter-spacing:-1px; margin-left:-3px;"};
+             _ -> #link{text=?_T("TOURNAMENTS"), url=?_U("/info-tournaments"), class="info-page-menu-elements info-page-tournaments-link", style="letter-spacing:-1px; margin-left:-3px;"}
+        end,
+        case En of
+             "/info-social" -> 
+                  #label{text=?_T("BE SOCIAL!"), class="info-page-menu-elements info-page-social-label"};
+             _ -> #link{text=?_T("BE SOCIAL!"), url=?_U("/info-social"), class="info-page-menu-elements info-page-social-link"}
+        end,
+        case En of
+             "/info-matchmaker" -> 
+                   #label{text=?_T("MATCHMAKER"), class="info-page-menu-elements info-page-matchmaker-label"};
+             _ ->  #link{text=?_T("MATCHMAKER"), url=?_U("/info-matchmaker"), class="info-page-menu-elements info-page-matchmaker-link"}
+        end,
+        case En of
+             "/info-why" -> 
+                   #label{text=?_T("MEMBERSHIP"), class="info-page-menu-elements info-page-membership-label"};
+             _ ->  #link{text=?_T("MEMBERSHIP"), url=?_U("/info-why"), class="info-page-menu-elements info-page-membership-link"}
+        end,
+        #link{text=?_T("SIGNUP!"), url=?_U("/login/register"), class="info-page-menu-elements info-page-join-link"}
+    ].
+
+content() ->   
     [
         menu(),
+        case site_utils:detect_language() of
+            "en" -> en();
+            "tr" -> tr()
+        end
+    ].
+
+
+tr() -> % PUBLIC BETA will have to redo this with css    
+    [
         #panel{class="info-page-header-panel", body=[
             #image{image="/images/more_info/info_gifts_bg.png", class="info-page-bg"},
             #panel{style="top:90px; left:230px; font-size:50px;",body=?_T("Üye Olduğunuz An")},
@@ -93,6 +152,54 @@ content() ->    % PUBLIC BETA will have to redo this with css
         #panel{class="info-page-content-panel", body=[
             #panel{style="top:1287px; left:444px; font-size:18px;",body=?_T("Sanal alemden satın alabileceğiniz")},
             #panel{style="top:1311px; left:468px; font-size:20px;",body=?_T("<b>çok geniş bir ürün yelpazesi</b> sizi bekliyor…")}
+        ]}
+    ].
+
+
+en() ->
+    [
+        #panel{class="info-page-header-panel", body=[
+            #image{image="/images/more_info/info_gifts_bg.png", class="info-page-bg"},
+            #panel{style="top:90px; left:230px; font-size:50px;",body=?_T("Claim your prize right at")},
+            #panel{style="top:145px; left:232px; font-size:48px;",body=?_T("the moment you register!")},
+            #panel{style="top:225px; left:230px; font-size:30px; color:#FC6404;",body=?_T("No more depositing with no results.")}
+        ]},
+        #panel{class="info-page-content-panel", body=[
+            #panel{style="top:375px; left:400px; font-size:25px;",body=?_T("Your time spent and purchases will")},
+            #panel{style="top:410px; left:440px; font-size:25px; color:#FC6404;",body=?_T("come back to you as prizes!")}
+        ]},
+        #panel{class="info-page-content-panel", body=[
+            #panel{style="top:635px; left:112px; font-size:20px; 
+                            transform:rotate(1.5deg);
+                            -ms-transform:rotate(1.5deg);
+                            -moz-transform:rotate(1.5deg);
+                            -webkit-transform:rotate(1.5deg);
+                            -o-transform:rotate(1.5deg);",
+            body=?_T("No matter if you win or lose, you will EARN the")},
+            #panel{style="top:668px; left:130px; font-size:25px; color:#FC6404;
+                            transform:rotate(1.5deg);
+                            -ms-transform:rotate(1.5deg);
+                            -moz-transform:rotate(1.5deg);
+                            -webkit-transform:rotate(1.5deg);
+                            -o-transform:rotate(1.5deg);",body=?_T("points to claim the prize you want.")},
+            #panel{style="top:715px; left:51px; font-size:17px;
+                            transform:rotate(1.5deg);
+                            -ms-transform:rotate(1.5deg);
+                            -moz-transform:rotate(1.5deg);
+                            -webkit-transform:rotate(1.5deg);
+                            -o-transform:rotate(1.5deg);",body=?_T("You still earn points when you lose a game, but earn much more if you win one.")}
+        ]},
+        #panel{class="info-page-content-panel", body=[
+            #panel{style="top:852px; left:460px; font-size:24px;",body=?_T("You can claim the prize you desire")},
+            #panel{style="top:885px; left:484px; font-size:24px;",body=?_T("with the points in your account")}
+        ]},
+        #panel{class="info-page-content-panel", body=[
+            #panel{style="top:1052px; left:82px; font-size:21px;",body=?_T("If you wish you can purchase a membership with")},
+            #panel{style="top:1082px; left:120px; font-size:21px;",body=?_T("prize points and achieve your goal faster!")}
+        ]},
+        #panel{class="info-page-content-panel", body=[
+            #panel{style="top:1287px; left:444px; font-size:18px;",body=?_T("Hundreds of prizes in our catalogue waiting to be claimed")},
+            #panel{style="top:1320px; left:522px; font-size:24px;",body=?_T("with the points you have won!")}
         ]}
     ].
 
--- apps/nsw_srv/src/info_social.erl
+++ apps/nsw_srv/src/info_social.erl
@@ -17,6 +17,14 @@ body() ->
 content() -> 
     [
         info_gifts:menu(),
+        case site_utils:detect_language() of
+            "en" -> en();
+            "tr" -> tr()
+        end
+    ].
+
+tr() ->
+    [
         #panel{class="info-page-header-panel", body=[
             #image{image="/images/more_info/info_social_bg.png", class="info-page-bg"},
             #panel{style="left:284px; top:104px; font-size:75px;",body=?_T("SOSYALLEŞMEK")},
@@ -54,6 +62,47 @@ content() ->
         ]}
     ].
 
+en() ->
+    [
+        #panel{class="info-page-header-panel", body=[
+            #image{image="/images/more_info/info_social_bg.png", class="info-page-bg"},
+            #panel{style="left:284px; top:104px; font-size:75px;",body=?_T("Socialize")},
+            #panel{style="left:285px; top:192px; font-size:20px; color:#FC6404;",body=?_T("This is not like any other place where you can just play a game.")},
+            #panel{style="left:286px; top:218px; font-size:20px; color:#FC6404;",body=?_T("It's a platform where you can share anything you like!")}
+        ]},
+        #panel{class="info-page-content-panel", body=[
+            #panel{style="left:543px; top:354px; font-size:16px;",body=?_T("Appear as you are or be as you appear.")},
+            #panel{style="left:520px; top:374px; font-size:16px;",body=?_T("Whatever you need to describe yourself, do so…")},
+            #panel{style="left:631px; top:396px; font-size:16px; color:#FC6404;",body=?_T("Your music,")},
+            #panel{style="left:631px; top:417px; font-size:18px; color:#FC6404;",body=?_T("your ideas,")},
+            #panel{style="left:604px; top:436px; font-size:26px; color:#FC6404;",body=?_T("your photos!")},
+            #panel{style="left:602px; top:466px; font-size:22px;",body=?_T("Inside and out!")},
+            #panel{style="left:592px; top:493px; font-size:16px; color:#FC6404;",body=?_T("You are welcome here!")}
+        ]},
+        #panel{class="info-page-content-panel", body=[
+            #panel{style="left:80px; top:607px; font-size:18px;",body=?_T("Create your world and only share with the people YOU want…")},
+            #panel{style="left:186px; top:636px; font-size:24px; color:#FC6404;",body=?_T("After all it's your world!")}
+        ]},
+        #panel{class="info-page-content-panel", body=[
+            #panel{style="left:452px; top:790px; font-size:18px;",body=?_T("Conversate, debate, hang out, comment or just spectate…")},
+            #panel{style="left:567px; top:814px; font-size:18px; color:#FC6404;",body=?_T("Like, be liked or be disliked.")},
+            #panel{style="left:566px; top:837px; font-size:18px;",body=?_T("It goes however you want it.")},
+            #panel{style="left:610px; top:862px; font-size:20px; color:#FC6404;",body=?_T("Keep in mind!")},
+            #panel{style="left:536px; top:888px; font-size:18px;",body=?_T("What <b>goes</b> around – <b>comes</b> around!")},
+            #panel{style="left:541px; top:933px; font-size:20px;",body=?_T("Your old mates, new buddies…")},
+            #panel{style="left:534px; top:958px; font-size:20px;",body=?_T("<b>Different</b> groups, <b>different</b> people.")}
+        ]},
+        #panel{class="info-page-content-panel", body=[
+            #panel{style="left:300px; top:1066px; font-size:22px;",body=?_T("Freedom of speech!")},
+            #panel{style="left:180px; top:1112px; font-size:20px; color:#FC6404;",body=?_T("Don't break the rules though, you will be prevented")},
+            #panel{style="left:355px; top:1140px; font-size:20px; color:#FC6404;",body=?_T("if you do.")}
+        ]},
+        #panel{class="info-page-content-panel", body=[
+            #panel{style="left:345px; top:1260px; font-size:20px;",body=?_T("You are not restricted to what you find in the game enviroment")},
+            #panel{style="left:568px; top:1286px; font-size:20px;",body=?_T("anymore…")},
+            #panel{style="left:470px; top:1324px; font-size:24px; color:#FC6404;",body=?_T("Create YOUR OWN world.")}
+        ]}
+    ].
 
 event(Any)->
     webutils:event(Any).
