95a7dbb71ddc0439691596c051bb54babe803857
Maxim Sokhatsky <maxim@synrc.com>
2012-10-07 22:45:45 +0300

    red build

--- apps/nsm_srv/src/nsm_srv_sup.erl
+++ apps/nsm_srv/src/nsm_srv_sup.erl
@@ -8,4 +8,10 @@ start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 init([]) ->
+  RestartStrategy = one_for_one,
+    MaxRestarts = 1000,
+    MaxSecondsBetweenRestarts = 3600,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
     {ok, {SupFlags, []}}.
