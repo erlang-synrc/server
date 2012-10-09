1bbc2101b7704cfe2f5dac4dbba57abb4b0a2295
Maxim Sokhatsky <maxim@synrc.com>
2012-10-09 16:27:16 +0300

    clean releases before issue

--- release.sh
+++ release.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+rm -rf rels/app/node/lib
+rm -rf rels/app/node/log
+rm -rf rels/app/node/releases
+rm -rf rels/game/node/lib
+rm -rf rels/game/node/log
+rm -rf rels/game/node/releases
+rm -rf rels/web/node/lib
+rm -rf rels/web/node/log
+rm -rf rels/web/node/releases
+
 cd rels/app
 ../../rebar -f generate
 cd ../game
