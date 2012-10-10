e8b6e4df3b98da9c2c240b034a2c19abd58505c3
Maxim Sokhatsky <maxim@synrc.com>
2012-10-10 09:49:04 +0300

    please pull & check before commit

--- apps/nsm_db/src/nsm_affiliates.erl
+++ apps/nsm_db/src/nsm_affiliates.erl
@@ -67,11 +67,7 @@
          disable_contract_type/1,
          disable_contract_type/2,
          invitation_hook/2,
-         purchase_hook/1,
-
-         write_affiliate_rel_record/2,
-         write_contract_record/2,
-         write_contract_type_record/2
+         purchase_hook/1
         ]).
 
 %% For test proposes
