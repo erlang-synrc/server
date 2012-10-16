3733612fea17bf65de32856c20b66daf1987c3b3
Alexandr Kalenuk <akalenuk@gmail.com>
2012-10-16 16:50:40 +0300

    Active users faster version (still have to reorganize the whole thing, this is just a temp)

--- apps/nsm_db/src/nsm_groups.erl
+++ apps/nsm_db/src/nsm_groups.erl
@@ -250,16 +250,19 @@ get_popular_groups(Count) ->  get_popular_groups("Lalala", Count).
 %
 get_active_members()      -> get_active_members(10).
 get_active_members(Count) ->
-    CurrentTime = now(),
-    Users = [U || #entry{from=U} <- nsm_db:select(entry,
-        fun(#entry{created_time=CT, type = ET}) when ET=:={user, normal};ET=:={user, group}->
-            case timer:now_diff(CurrentTime, CT) of
-                M when M < 2592000000000 -> true  % 30 days in microseconds
-                ;_                       -> false
-            end
-           ;(_)->false end)],
-    Dict = lists:foldl(fun(U, A)->dict:update(U, fun(V) -> V+1 end, 1, A) end, dict:new(), Users),
-    lists:sublist(lists:sort(fun({_,N1},{_,N2}) when N1 > N2-> true;(_,_)->false end, dict:to_list(Dict)), Count).
+    Users = [UI#user.username || UI <- nsm_db:all(user)],
+    UserActivity = lists:sort( fun({_, X1}, {_, X2}) -> X1 >= X2 end, [{U, feed:get_entries_count(U)} || U <- Users]),
+    lists:sublist(UserActivity, Count).
+%    CurrentTime = now(),
+%    Users = [U || #entry{from=U} <- nsm_db:select(entry,
+%        fun(#entry{created_time=CT, type = ET}) when ET=:={user, normal};ET=:={user, group}->
+%            case timer:now_diff(CurrentTime, CT) of
+%                M when M < 2592000000000 -> true  % 30 days in microseconds
+%                ;_                       -> false
+%            end
+%           ;(_)->false end)],
+%    Dict = lists:foldl(fun(U, A)->dict:update(U, fun(V) -> V+1 end, 1, A) end, dict:new(), Users),
+%    lists:sublist(lists:sort(fun({_,N1},{_,N2}) when N1 > N2-> true;(_,_)->false end, dict:to_list(Dict)), Count).
 
 %%% ====================================
 %%% web api
