e2253dcc846a00125d05e61ad81c9aec750913ab
Maxim Sokhatsky <maxim@synrc.com>
2012-10-25 02:33:34 +0300

    clean mnesia tables

--- release.sh
+++ release.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 rm -rf rels/app/node/lib
+rm -rf rels/app/node/data
 rm -rf rels/app/node/log
 rm -rf rels/app/node/releases
 rm -rf rels/game/node/lib
+rm -rf rels/game/node/data
 rm -rf rels/game/node/log
 rm -rf rels/game/node/releases
 rm -rf rels/web/node/lib
 rm -rf rels/web/node/log
+rm -rf rels/web/node/data
 rm -rf rels/web/node/releases
 
 cd rels/app
