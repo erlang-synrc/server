133767ce5eb98d49fc0af1cfeb5593566f2d575c
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2012-10-27 16:31:46 +0300

    Update the MIBs with Synrc private IANA enterprise number.

--- apps/nsx_mibs/src/mem_mib.erl
+++ apps/nsx_mibs/src/mem_mib.erl
@@ -1,10 +1,12 @@
 -module(mem_mib).
 
--export([mem_table/1, mem_table/3]).
--export([update_mem_table/0]).
+-export([load/0, mem_table/1, mem_table/3, update_mem_table/0]).
 
 -record(memTable, {id, total, used, memStatus}).
 -define(memShadowArgs, {memTable, integer, record_info(fields, memTable), 5000, fun mem_mib:update_mem_table/0}).
+
+load()->
+    snmpa:load_mibs(snmp_master_agent, [code:priv_dir(nsx_mibs)++"/mibs"++"/MEM-MIB"]).
 
 mem_table(Op)->
     snmp_shadow_table:table_func(Op, ?memShadowArgs).
