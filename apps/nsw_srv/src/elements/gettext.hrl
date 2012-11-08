1b9859624a0ef9302855915d1481ed08c866434e
Maxim Sokhatsky <maxim@synrc.com>
2012-11-08 21:26:35 +0200

    VERSION

--- apps/nsw_srv/src/elements/gettext.hrl
+++ apps/nsw_srv/src/elements/gettext.hrl
@@ -1,1 +1,5 @@
-../../include/gettext.hrl
+-include_lib("gettext/include/gettext.hrl").
+
+-define(_T(S), gettext:key2str(S, site_utils:detect_language())).
+-define(_TS(S, A), gettext_format:stxt(gettext:key2str(S, site_utils:detect_language()),A)).
+-define(_U(U), uri_translator:translate(U, "en", site_utils:detect_language())).
--- single_configure.sh
+++ single_configure.sh
@@ -4,9 +4,9 @@ LOCAL_IP=${1:-"`hostname -i`"}
 APP=${2:-"app@`hostname -f`"}
 
 cd rels/app/node/etc
-./configure -dba nsm_riak -app $APP -game game -web web -mq-user guest -mq-pass guest
+./configure -dba nsm_riak -app app -game game -web web -mq-user guest -mq-pass guest
 cd ../../../game/node/etc
-./configure -app $APP -game game -game-port 9000 -game-host $LOCAL_IP -web web -mnesia-init -mq-user guest -mq-pass guest -sync true
+#./configure -app $APP -game game -game-port 9000 -game-host $LOCAL_IP -web web -mnesia-init -mq-user guest -mq-pass guest -sync true
 cd ../../../web/node/etc
-./configure -ip $LOCAL_IP -app $APP -web web -web-port 8000 -game game -srv $LOCAL_IP -srv-host $LOCAL_IP -srv-port 9000 -mq-user guest -mq-pass guest -fb-app-id 154227314626053 -fb-app-secret cf9d49958ee536dd75f15bf8ca541965 -jspack full -csspack full -sync true
+#./configure -ip $LOCAL_IP -app $APP -web web -web-port 8000 -game game -srv $LOCAL_IP -srv-host $LOCAL_IP -srv-port 9000 -mq-user guest -mq-pass guest -fb-app-id 154227314626053 -fb-app-secret cf9d49958ee536dd75f15bf8ca541965 -jspack full -csspack full -sync true
 cd ../../../..
