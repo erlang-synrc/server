51f65bf9e22edf1f765aa46f4ba00a9fb50d5b55
Maxim Sokhatsky <maxim@synrc.com>
2012-11-09 18:47:43 +0200

    revert

--- apps/nsw_srv/src/elements/gettext.hrl
+++ apps/nsw_srv/src/elements/gettext.hrl
@@ -1,5 +1,1 @@
--include_lib("gettext/include/gettext.hrl").
-
--define(_T(S), gettext:key2str(S, site_utils:detect_language())).
--define(_TS(S, A), gettext_format:stxt(gettext:key2str(S, site_utils:detect_language()),A)).
--define(_U(U), uri_translator:translate(U, "en", site_utils:detect_language())).
+../../include/gettext.hrl
