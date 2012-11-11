964b2a27773d65868bb6f55b1d52d8c664e4e837
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2012-11-11 02:10:14 +0200

    add order processing stubs to fb callback

--- apps/nsw_srv/src/buy_facebook.erl
+++ apps/nsw_srv/src/buy_facebook.erl
@@ -9,13 +9,12 @@
 
 main() ->
     wf:content_type("application/json"),
-    {ok, Data} = fb_signed_request:parse(wf:q(signed_request), ?FB_APP_SECRET), %?FB_APP_SECRET "df0ed1f649bf974189947caf832ffa01"
+    {ok, Data} = fb_signed_request:parse(wf:q(signed_request), ?FB_APP_SECRET), % "df0ed1f649bf974189947caf832ffa01"
     SignedReq = mochijson2:decode(Data),
-    wf:info("signed_request:~p~n",[SignedReq]),
+    %wf:info("signed_request:~p~n",[SignedReq]),
     Response = #struct{list=[
-        {<<"content">>, process_order(wf:q(method), proplists:get_value(<<"credits">>, SignedReq#struct.list))},
-        {<<"method">>, list_to_binary(wf:q(method))}]},
-    wf:info("Response: ~p~n", [Response]),
+	{<<"content">>, process_order(wf:q(method), proplists:get_value(<<"credits">>, SignedReq#struct.list))},
+	{<<"method">>, list_to_binary(wf:q(method))}]},
     mochijson2:encode(Response).
 
 process_order("payments_get_items", Credits)->
@@ -28,15 +27,47 @@ process_order("payments_get_items", Credits)->
 	{<<"image_url">>, <<"http:\/\/www.facebook.com\/images\/gifts\/21.png">>},
 	{<<"description">>, list_to_binary(
 	    ["Gift points :", integer_to_list(GiftPoints),
-	    " Net membership: ", integer_to_list(NetMembership)])}]}
+	    " Net membership: ", integer_to_list(NetMembership)])},
+	{<<"item_id">>, list_to_binary(ItemId)}]}
 	|| #membership_package{id = Id, no=No,
 	    amount=Amount,
 	    deducted_for_gifts=GiftPoints,
 	    net_membership=NetMembership} <- Packages, Id=:=ItemId];
 process_order("payments_status_update", Credits)->
-    OrderId = proplists:get_value(<<"order_id">>, Credits#struct.list),
-    OrderDetails = proplists:get_value(<<"order_details">>, Credits#struct.list),
-    wf:info("Order details: ~p~n", [OrderDetails]),
-    #struct{list=[{<<"status">>, <<"settled">>}, {<<"order_id">>, OrderId}]}.
+    OrderDetails = mochijson2:decode(proplists:get_value(<<"order_details">>, Credits#struct.list)),
+    Response = order_status(proplists:get_value(<<"status">>, OrderDetails#struct.list), OrderDetails#struct.list),
+    Response.
+
+order_status(<<"placed">>, OrderDetails)->
+    wf:info("order placed ~p~n", [OrderDetails]),
+    OrderId = proplists:get_value(<<"order_id">>, OrderDetails),
+    Items = proplists:get_value(<<"items">>, OrderDetails),
+    Item = lists:nth(1, Items),
+    PackageId = proplists:get_value(<<"item_id">>, Item#struct.list),
+    wf:info("Process package id ~p~n ", [PackageId]),
+
+    % nsm_membership_packages:check_limit_over(UId, PackageId)
+    % buy:over_limit_popup(nsm_membership_packages:get_monthly_purchase_limit());
+
+    NextStatus = <<"settled">>, % <<"canceled">>
+    #struct{list=[{<<"status">>, NextStatus}, {<<"order_id">>, OrderId}]};
+order_status(<<"disputed">>, OrderDetails)->
+    wf:info("order disputed: ~p~n", [OrderDetails]),
+    % Track disputed item orders.
+    % Investigate user's dispute and resolve by settling or refunding the order.
+    % Update the order status asychronously using Graph API.
+    [];
+order_status(<<"refunded">>, OrderDetails)->
+    wf:info("order refunded ~p~n", [OrderDetails]),
+    % Track refunded item orders initiated by Facebook. No need to respond.
+    [];
+order_status(<<"settled">>, OrderDetails)->
+    wf:info("order settled ~p~n", [OrderDetails]),
+    OrderId = proplists:get_value(<<"order_id">>, OrderDetails),
+    % Verify that the order ID corresponds to a purchase fullfilled
+    #struct{list=[{<<"status">>, <<"settled">>}, {<<"order_id">>, OrderId}]};
+order_status(OrderStatus, OrderDetails)->
+    wf:info("buy_facebook: Order satus ~p, details: ~p~n", [OrderStatus, OrderDetails]),
+    [].
 
 event(_)-> ok.
