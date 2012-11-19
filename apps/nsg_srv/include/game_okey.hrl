bf5df904ba65756ec91d25b5c18c07d086aa8f4e
Sergei Polkovnikov <serge.polkovnikov@gmail.com>
2012-11-19 19:36:12 +0200

    Okey: Remove debug data from the okey_game_info

--- apps/nsg_srv/include/game_okey.hrl
+++ apps/nsg_srv/include/game_okey.hrl
@@ -110,8 +110,7 @@
           observer_flag :: boolean(),
           pause_enabled = true :: boolean(),
           social_actions_enabled = true :: boolean(),
-          tournament_type = standalone :: tournament(),
-          xxx           :: term() %% For test propouses only. Remove This!!!
+          tournament_type = standalone :: tournament()
          }).
 -record(okey_player_ready, {
           player :: 'PlayerId'()
