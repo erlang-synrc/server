1322e3a0de341a0db103273069ce4b8b73454bc1
Alexandr Kalenuk <akalenuk@gmail.com>
2012-11-25 09:14:58 +0200

    Plug kakara.min.css only from srv3

--- apps/nsw_srv/src/test_iebug.erl
+++ apps/nsw_srv/src/test_iebug.erl
@@ -23,17 +23,33 @@ main() ->"
 	<title>Kakaranet</title>
 	<link rel='stylesheet' type='text/css' href='http://srv3.kakaranet.com/css/kakara.min.css' media='all' />
 	<link rel='stylesheet' href='http://srv3.kakaranet.com/css/tr.css' />
-	<script src='http://srv3.kakaranet.com/js/kakara.min.js' type='text/javascript' charset='utf-8'></script>
+	<script src='/js/kakara.min.js' type='text/javascript' charset='utf-8'></script>
+	<!--[if IE]>
+		<link rel='stylesheet' type='text/css' href='/css/ie.css' media='all'/>
+	<![endif]-->
+	<!--[if lt IE 7]>
+		<script src='/nitrogen/js/ie-hover.js' type='text/javascript'></script>
+		<script src='/nitrogen/js/ie-png.js' type='text/javascript'></script>
+	<![endif]-->
+	<!--[if lt IE 9]>
+		<script src='/nitrogen/js/html5.js' type='text/javascript'></script>
+		<link rel='stylesheet' type='text/css' href='/css/ie7.css' media='all'/>
+	<![endif]-->
+	<!--[if IE 9]>
+		<link rel='stylesheet' type='text/css' href='/css/ie9.css' media='all' />
+	<![endif]-->
+	<!-- Most of scripts and css files were wiped out, you can see old state of this file by command
+	git cat-file blob 4a15d19e2b760379d3776a95fcb84bac9b86fab3 -->
 	
-    <link href='http://srv3.kakaranet.com/nitrogen/video-js/video-js.css' rel='stylesheet'>
-    <script src='http://srv3.kakaranet.com/nitrogen/video-js/video.js'></script>
+    <link href='/nitrogen/video-js/video-js.css' rel='stylesheet'>
+    <script src='/nitrogen/video-js/video.js'></script>
 
-    <link href='http://srv3.kakaranet.com/nitrogen/guiders-js/guiders-1.2.8.css' rel='stylesheet'>
-    <script src='http://srv3.kakaranet.com/nitrogen/guiders-js/guiders-1.2.8.js'></script>
+    <link href='/nitrogen/guiders-js/guiders-1.2.8.css' rel='stylesheet'>
+    <script src='/nitrogen/guiders-js/guiders-1.2.8.js'></script>
 
-    <script src='http://srv3.kakaranet.com/nitrogen/js/form.js' type='text/javascript'></script>
-    <script src='http://srv3.kakaranet.com/nitrogen/swfobject.js' type='text/javascript' charset='utf-8'></script>
-    <script src='http://srv3.kakaranet.com/nitrogen/audio-player/audio-player.js' type='text/javascript' charset='utf-8'></script>
+    <script src='/nitrogen/js/form.js' type='text/javascript'></script>
+    <script src='/nitrogen/swfobject.js' type='text/javascript' charset='utf-8'></script>
+    <script src='/nitrogen/audio-player/audio-player.js' type='text/javascript' charset='utf-8'></script>
     <script type='text/javascript'>
 
         AudioPlayer.setup('/nitrogen/audio-player/player.swf', {width: 290});
@@ -201,9 +217,9 @@ main() ->"
 
         });
 	</script>
-	<script src='http://srv3.kakaranet.com/nitrogen/js/jquery.serialScroll-1.2.2-min.js' type='text/javascript' charset='utf-8'></script>
-	<script src='http://srv3.kakaranet.com/nitrogen/js/jquery.scrollTo-1.4.2-min.js' type='text/javascript' charset='utf-8'></script>
-	<script src='http://srv3.kakaranet.com/nitrogen/js/jquery.autosize-min.js' type='text/javascript' charset='utf-8'></script>
+	<script src='/nitrogen/js/jquery.serialScroll-1.2.2-min.js' type='text/javascript' charset='utf-8'></script>
+	<script src='/nitrogen/js/jquery.scrollTo-1.4.2-min.js' type='text/javascript' charset='utf-8'></script>
+	<script src='/nitrogen/js/jquery.autosize-min.js' type='text/javascript' charset='utf-8'></script>
 
 </head>
 <body class='info_gifts'>
