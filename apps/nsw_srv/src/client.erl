4c1105722195a39d334718939252825bb167f4d5
SRV2 user <kakauser@srv2.kakaranet.com>
2012-12-07 04:46:59 +0100

    client goes to game server depends on hosted game

--- apps/nsw_srv/src/client.erl
+++ apps/nsw_srv/src/client.erl
@@ -45,16 +45,18 @@ token() ->
         io_lib:fwrite("flashvars.tokenKey = encodeURIComponent(\"~s\");~n", [T]),
         io_lib:fwrite("flashvars.port = ~b;~n", [?SERVER_PORT]),
         io_lib:fwrite("flashvars.locale = '~s';~n", [site_utils:detect_language()]),
-        io_lib:fwrite("flashvars.host = \"~s\";~n", [case ?SERVER_HOST of
-                                                        "188.40.111.154" -> "test.kakaranet.com";
-                                                        X -> X
-                                                  end]),
+%        io_lib:fwrite("flashvars.host = \"~s\";~n", [case ?SERVER_HOST of
+%                                                        "188.40.111.154" -> "test.kakaranet.com";
+%                                                        X -> X
+%                                                  end]),
         io_lib:fwrite(GameType, []),
 
         case wf:q(id) of
              undefined -> "";
              Id ->        A = wf:session(Id),
                           GameId = case A of undefined -> Id; _ -> list_to_integer(A) end,
+                          io_lib:fwrite("flashvars.host = ~p;~n", [ "srv" ++ integer_to_list(GameId div 1000000) ++
+                                                                      ".kakaranet.com"])
                           io_lib:fwrite("flashvars.gameId = ~p;~n", [GameId])
         end
     ].
