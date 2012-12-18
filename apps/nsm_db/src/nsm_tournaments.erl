1d1bf2e0271ba2b57dcc057f65a4cbe2359b7858
Maxim Sokhatsky <maxim@synrc.com>
2012-12-18 16:22:27 +0200

    clear tournaments functions

--- apps/nsm_db/src/nsm_tournaments.erl
+++ apps/nsm_db/src/nsm_tournaments.erl
@@ -1,22 +1,7 @@
 -module(nsm_tournaments).
-
 -include("user.hrl").
 -include("tournaments.hrl").
-
--export([create/2,create/13,     % create the tournament
-         destroy/1,              % purge tournament with all team placeholders and played games
-         join/2,                 % put users in waiting queue
-         waiting_player/1,       % dequeue first waiting_player
-         start/1,                % move users from wating queue to teams
-         remove/2,               % unsubscribe user from tournaments in all sences
-         all/0,                  % return all registered tournaments
-         joined_users/1,         % all wating and active (if tournament started) users
-         user_tournaments/1,     % all tournaments in which user participates
-         list_users_per_team/1,  % list of played games of a team with all user replacements
-         get/1,                  % get tournament from db
-         user_is_team_creator/2, % if user have team creator priority
-         create_team/1,
-         user_joined/2]).        % if user is already joined the tournament
+-compile(export_all).
 
 create_team(Name) ->
     TID = nsm_db:next_id("team",1),
@@ -56,7 +41,6 @@ get(TID) ->
     end.
 
 start(_TID) -> ok.
-destroy(_TID) -> erlang:error(notimpl).
 join(UID, TID) -> nsm_db:join_tournament(UID, TID).
 remove(UID, TID) -> nsm_db:leave_tournament(UID, TID).
 waiting_player(TID) -> nsm_db:tournament_pop_waiting_player(TID).
@@ -68,3 +52,7 @@ user_joined(TID, UID) ->
 all() -> nsm_db:all(tournament).
 user_is_team_creator(_UID, _TID) -> true.
 list_users_per_team(_TeamID) -> [].
+destroy(TID) -> nsm_db:delete_by_index(play_record, <<"play_record_tournament_bin">>, TID),
+                          nsm_db:delete(tournament,TID).
+clear() -> [destroy(T#tournament.id) || T <- nsm_db:all(tournament)].
+lost() -> lists:usort([erlang:element(3, I) || I <- nsm_db:all(play_record)]).
