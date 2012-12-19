d6990c148d10f8a67671c6e1f60809ccefee2bb2
Alexandr Kalenuk <akalenuk@gmail.com>
2012-12-19 10:02:42 +0200

    Move info pages to html templates

--- apps/nsw_srv/src/info_gifts.erl
+++ apps/nsw_srv/src/info_gifts.erl
@@ -121,88 +121,11 @@ content() ->
     ].
 
 
-tr() -> % PUBLIC BETA will have to redo this with css    
-    [
-        #panel{class="info-page-header-panel", body=[
-            #image{image="/images/more_info/info_gifts_bg.png", class="info-page-bg"},
-            #panel{style="top:90px; left:230px; font-size:50px;",body=("Üye Olduğunuz An")},
-            #panel{style="top:145px; left:225px; font-size:70px;",body=("Hediyenizi Alın!")},
-            #panel{style="top:225px; left:230px; font-size:30px; color:#FC6404;",body=("Artık karşılıksız verme devri bitti...")}
-        ]},
-        #panel{class="info-page-content-panel", body=[
-            #panel{style="top:385px; left:400px; font-size:18px;",body=("Bundan sonra oynamaya ayırdığınız")},
-            #panel{style="top:410px; left:430px; font-size:25px; color:#FC6404;",body=("zaman ve yaptığınız harcamalar")},
-            #panel{style="top:444px; left:460px; font-size:18px;",body=("size <b>hediye</b> olarak geri dönüyor. ")}
-        ]},
-        #panel{class="info-page-content-panel", body=[
-            #panel{style="top:627px; left:331px; font-size:18px;",body=("Üyelerimiz, hiçbir şarta bağlı")},
-            #panel{style="top:651px; left:237px; font-size:18px;",body=("olmadan hediye puanları kazanırlar.")},
-            #panel{style="top:674px; left:51px; font-size:25px; color:#FC6404;",body=("Kazanmak yada kaybetmek fark etmez.")},
-            #panel{style="top:705px; left:144px; font-size:18px;",body=("Oyun oynayan her oyuncu hediye puanı kazanır.")},
-            #panel{style="top:730px; left:72px; font-size:18px;",body=("Kaybeden de kazanır, kazanan daha da çok kazanır.")}
-        ]},
-        #panel{class="info-page-content-panel", body=[
-            #panel{style="top:842px; left:420px; font-size:18px;",body=("Hesabında biriken puanlar ile")},
-            #panel{style="top:867px; left:439px; font-size:20px;",body=("hediye kataloğundan <b>istediği</b> hediyeyi alabilir.")}
-        ]},
-        #panel{class="info-page-content-panel", body=[
-            #panel{style="top:1052px; left:209px; font-size:18px;",body=("İsterse, puanlı üyelik paketi satın alarak")},
-            #panel{style="top:1078px; left:186px; font-size:20px;",body=("daha çok <b>puan</b> kazanır.")}
-        ]},
-        #panel{class="info-page-content-panel", body=[
-            #panel{style="top:1287px; left:444px; font-size:18px;",body=("Sanal alemden satın alabileceğiniz")},
-            #panel{style="top:1311px; left:468px; font-size:20px;",body=("<b>çok geniş bir ürün yelpazesi</b> sizi bekliyor…")}
-        ]}
-    ].
-
+tr() ->
+    #template { file=code:priv_dir(nsw_srv)++"/templates/info_gifts_tr.html" }.
 
 en() ->
-    [
-        #panel{class="info-page-header-panel", body=[
-            #image{image="/images/more_info/info_gifts_bg.png", class="info-page-bg"},
-            #panel{style="top:90px; left:230px; font-size:50px;",body=("Claim your prize right at")},
-            #panel{style="top:145px; left:232px; font-size:48px;",body=("the moment you register!")},
-            #panel{style="top:225px; left:230px; font-size:30px; color:#FC6404;",body=("No more depositing with no results.")}
-        ]},
-        #panel{class="info-page-content-panel", body=[
-            #panel{style="top:375px; left:400px; font-size:25px;",body=("Your time spent and purchases will")},
-            #panel{style="top:410px; left:440px; font-size:25px; color:#FC6404;",body=("come back to you as prizes!")}
-        ]},
-        #panel{class="info-page-content-panel", body=[
-            #panel{style="top:635px; left:112px; font-size:20px; 
-                            transform:rotate(1.5deg);
-                            -ms-transform:rotate(1.5deg);
-                            -moz-transform:rotate(1.5deg);
-                            -webkit-transform:rotate(1.5deg);
-                            -o-transform:rotate(1.5deg);",
-            body=("No matter if you win or lose, you will EARN the")},
-            #panel{style="top:668px; left:130px; font-size:25px; color:#FC6404;
-                            transform:rotate(1.5deg);
-                            -ms-transform:rotate(1.5deg);
-                            -moz-transform:rotate(1.5deg);
-                            -webkit-transform:rotate(1.5deg);
-                            -o-transform:rotate(1.5deg);",body=("points to claim the prize you want.")},
-            #panel{style="top:715px; left:51px; font-size:17px;
-                            transform:rotate(1.5deg);
-                            -ms-transform:rotate(1.5deg);
-                            -moz-transform:rotate(1.5deg);
-                            -webkit-transform:rotate(1.5deg);
-                            -o-transform:rotate(1.5deg);",body=("You still earn points when you lose a game, but earn much more if you win one.")}
-        ]},
-        #panel{class="info-page-content-panel", body=[
-            #panel{style="top:852px; left:460px; font-size:24px;",body=("You can claim the prize you desire")},
-            #panel{style="top:885px; left:484px; font-size:24px;",body=("with the points in your account")}
-        ]},
-        #panel{class="info-page-content-panel", body=[
-            #panel{style="top:1052px; left:82px; font-size:21px;",body=("If you wish you can purchase a membership with")},
-            #panel{style="top:1082px; left:120px; font-size:21px;",body=("prize points and achieve your goal faster!")}
-        ]},
-        #panel{class="info-page-content-panel", body=[
-            #panel{style="top:1287px; left:444px; font-size:18px;",body=("Hundreds of prizes in our catalogue waiting to be claimed")},
-            #panel{style="top:1320px; left:522px; font-size:24px;",body=("with the points you have won!")}
-        ]}
-    ].
-
+    #template { file=code:priv_dir(nsw_srv)++"/templates/info_gifts_en.html" }.
 
 event(Any)->
     webutils:event(Any).
