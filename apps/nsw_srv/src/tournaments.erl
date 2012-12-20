022b6f46b6b7a79c0aa4e8786755e851fc949f1c
Maxim Sokhatsky <maxim@synrc.com>
2012-12-20 20:01:07 +0200

    fix

--- apps/nsw_srv/src/tournaments.erl
+++ apps/nsw_srv/src/tournaments.erl
@@ -743,13 +743,9 @@ send_email(TID) ->
     MainGift = lists:nth(1,Gifts),
     ?INFO("Main Gift: ~p",[MainGift]),
      [ begin
-         try
             Mail = User#user.email,
             Username = User#user.username,
        {Subject, PlainText} = mail_construction:tournament(Username, Mail, Date, Time, MainGift, Tournament),
             ?INFO("Mail ~p",[{User#user.email,Subject,PlainText}]),
             nsx_msg:notify_email(Subject, PlainText, Mail)
-         catch Error:Reason
-            ?INFO("Error: ~p",[{Error,Reason}])
-         end
      end || User <- Users].
