5440b197a928b469b989d923c66224d9f3a7da4d
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2012-12-27 15:44:18 +0200

    add kindex.js

---
+++ apps/nsw_srv/priv/static/js/kindex.js
@@ -0,0 +1,9 @@
+// @depends ../nitrogen/js/jquery-1.6.2.js
+// @depends ../nitrogen/nitrogen.js
+// @depends ../nitrogen/bert.js
+// @depends ../nitrogen/js/jquery.cycle.all.js
+// @depends ../nitrogen/qtip/jquery.qtip.pack.js
+// @depends ../nitrogen/qtip.custom.js
+// @depends ../kaka/index.js
+
+
