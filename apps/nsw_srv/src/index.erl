721096edbb85b9f273c7d880b9d2c4001b238c9e
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2012-12-27 13:32:14 +0200

    slider js

--- apps/nsw_srv/src/index.erl
+++ apps/nsw_srv/src/index.erl
@@ -7,55 +7,7 @@
 -include("elements/records.hrl").
 -include("setup.hrl").
 
-main() ->
-  webutils:add_raw("<script type=\"text/javascript\">
-  var slides = [
-    '<img src=\"/images/slides/en/slide4.png\">',
-    '<img src=\"/images/slides/en/slide3.png\">',
-    '<img src=\"/images/slides/en/slide2.png\">'
-  ];
-  function loadedSlide(){
-    $('.slideshow').cycle('resume');
-  };
-
-  function beforeSlide(current, next, options, forward){
-    // works only from second item
-    var img = $(next).attr('src')[0];
-    if (slides[0] && options.addSlide) {
-      options.addSlide(slides.pop());};
-    };
-
-  function afterSlide(current, next, options, forward){
-    var img = $('.slideshow').find('img')[options.nextSlide];
-    if (img.complete !=true) {
-      $('.slideshow').cycle('pause');
-      loadingTimer = +new Date();
-      $(img).bind(\"load\",loadedSlide);
-    };
-    if (slides[0] && options.addSlide) {
-      options.addSlide(slides.pop());
-    };
-  };
-
-  $(document).ready(function() {
-  $('.slideshow').append(slides.pop()).cycle({
-    fx:     'fade',
-    pause: true,
-    prev:   '.pager .prev',
-    next:   '.pager .next',
-    pager:  '.switcher',
-    after:    afterSlide,
-    before:   beforeSlide,
-    autostop: false,
-    autostopCount: slides.length+2,
-    timeout: 200,
-    pagerAnchorBuilder: function(idx, slide) {
-      return '.switcher li:eq(' + idx + ') a';
-    }
-  });
-  });
-  </script>"),
-  #template { file=code:priv_dir(nsw_srv)++"/templates/index.html"}.
+main() -> #template { file=code:priv_dir(nsw_srv)++"/templates/index.html"}.
 
 title() -> "Kakaranet Okey".
 
@@ -77,35 +29,17 @@ body() ->
     undefined -> ok;
     Message -> show_message(Message)
   end,
-  Slides = case site_utils:detect_language() of
-    "tr" -> [
-%      {"BAŞLIYORUZ", "slide5_tr"},
-%      {"HEMEN KATILIN", "slide6_tr"},
-      {"Hedİyeler", "slide1"},
-      {"Turnuvalar", "slide2"},
-      {"Sosyalleşİn!", "slide3"},
-      {"Nasıl İstİyorsanız Öyle", "slide4"}];
-    _ -> [
-      {"Gifts", "slide1"},
-      {"Tournaments", "slide2"},
-      {"Be Social!", "slide3" },
-      {"Matchmaker", "slide4"}
-    ]
-  end,
   [
   #panel{class="page-content", body=[
-    "<div class=\"slideshow\"><img src=\"/images/slides/en/slide1.png\"></div>"
-%    #list{class=slideshow, body=[
-%      #listitem{class="slide "++S, body=[
-%        #panel{class=btns, body=[
-%          #link{text=?_T("More Info"), class="btn-dark", url=?_U("/info-gifts")}, %Detaylı Bilgi
-%          #link{text=?_T("LET'S PLAY!"), class="btn-yellow", url=?_U("/login/register")} %ÜYE OL!
-%        ]}
-%      ]} || {_,S} <- Slides]}
+    #panel{class=slideshow, body="<img src=\"/images/slides/"++site_utils:detect_language()++"/slide1.png\">"},
+    #panel{class=btns, body=[
+      #link{text=?_T("More Info"), class="btn-dark", url=?_U("/info-gifts")}, %Detaylı Bilgi
+      #link{text=?_T("LET'S PLAY!"), class="btn-yellow", url=?_U("/login/register")} %ÜYE OL!
+    ]}
   ]},
   #panel{class="slideshow-control", body=[
     #panel{class="page-content", body=[
-    #list{class=switcher, body=[#listitem{body=#link{text=?_T(L)}} || {L,_} <- Slides]},
+    #list{class=switcher, body=[#listitem{body=#link{text=?_T(L)}} || L <- ["Gifts", "Tournaments", "Be Social!", "Matchmaker"]  ]},
     #list{class=pager, body=[
       #listitem{body=#link{class=prev, text="prev"}},
       #listitem{body=#link{class=next, text="next"}}
