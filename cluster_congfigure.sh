60f4ef7fa5a8f746f3db94c6c550aae7c79b8cc2
Maxim Sokhatsky <maxim@synrc.com>
2013-01-02 12:06:43 +0200

    srv3 config for public

--- cluster_congfigure.sh
+++ cluster_congfigure.sh
@@ -1,34 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 NODE=`hostname -a`
 
-cp prod/$NODE/app.config rels/app/node/etc/app.config
-cp prod/$NODE/game.config rels/game/node/etc/app.config
-cp prod/$NODE/web.config rels/web/node/etc/app.config
-cp prod/$NODE/public.config rels/public/node/etc/app.config
+function setup_node {
+   cp prod/$NODE/$1.config rels/$1/node/etc/app.config
+   cp prod/$NODE/$1.vm.args rels/$1/node/etc/vm.args
+   cp prod/$NODE/$1.snmp.agent.conf rels/$1/node/etc/snmp/agent/agent.conf
+   cp prod/$NODE/$1.snmp.standard.conf rels/$1/node/etc/snmp/agent/standard.conf
+   cp prod/$NODE/$1.snmp.usm.conf rels/$1/node/etc/snmp/agent/usm.conf
+}
 
-cp prod/$NODE/app.vm.args rels/app/node/etc/vm.args
-cp prod/$NODE/game.vm.args rels/game/node/etc/vm.args
-cp prod/$NODE/web.vm.args rels/web/node/etc/vm.args
-cp prod/$NODE/public.vm.args rels/public/node/etc/vm.args
+setup_node app
+setup_node game
+setup_node web
+setup_node public
 
 cp prod/$NODE/webmachine.config rels/public/node/etc/webmachine.config
-
-cp prod/$NODE/app.snmp.agent.conf rels/app/node/etc/snmp/agent/agent.conf
-cp prod/$NODE/app.snmp.standard.conf rels/app/node/etc/snmp/agent/standard.conf
-cp prod/$NODE/app.snmp.usm.conf rels/app/node/etc/snmp/agent/usm.conf
-
-cp prod/$NODE/game.snmp.agent.conf rels/game/node/etc/snmp/agent/agent.conf
-cp prod/$NODE/game.snmp.standard.conf rels/game/node/etc/snmp/agent/standard.conf
-cp prod/$NODE/game.snmp.usm.conf rels/game/node/etc/snmp/agent/usm.conf
-
-cp prod/$NODE/web.snmp.agent.conf rels/web/node/etc/snmp/agent/agent.conf
-cp prod/$NODE/web.snmp.standard.conf rels/web/node/etc/snmp/agent/standard.conf
-cp prod/$NODE/web.snmp.usm.conf rels/web/node/etc/snmp/agent/usm.conf
-
-cp prod/$NODE/public.snmp.agent.conf rels/public/node/etc/snmp/agent/agent.conf
-cp prod/$NODE/public.snmp.standard.conf rels/public/node/etc/snmp/agent/standard.conf
-cp prod/$NODE/public.snmp.usm.conf rels/public/node/etc/snmp/agent/usm.conf
 
 FILES=rels/public/node/lib/nsp_srv-1/priv/static/files
 rm -rf $FILES
