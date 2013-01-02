d9b3e2261e3373b77e35649dbe3979d072013f71
SRV1 <kakauser@srv1.kakaranet.com>
2013-01-02 17:44:50 +0100

    srv1 configs

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ ln -s /mnt/glusterfs/kakafiles $FILES
 
 rels/app/node/bin/ns_node start
 rels/game/node/bin/ns_node start
-rels/web/node/bin/ns_node start
+#rels/web/node/bin/ns_node start
 rels/public/node/bin/ns_node start
 
 chmod -R o+rX apps
