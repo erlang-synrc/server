95beb3a02b97f14546ddd4d591b94eac8ee16525
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-01-11 21:15:32 +0200

    css corrections

---
+++ apps/nsp_srv/priv/static/css/cssfix.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+host=`hostname -s`
+static="url("
+
+if [ ${host}  == "srv1" ]
+then
+  static="url(\/\/static1.kakaranet.com"
+elif [ $host  == "srv2" ]
+then
+  static="url(\/\/static2.kakaranet.com"
+elif [ $host  == "srv3" ]
+then
+  static="url(\/\/static3.kakaranet.com"
+fi
+
+declare -A replacements=(
+#greyscale filter values
+["values=..10000,01000,00100,00010.."]="values=\\\'1 0 0 0 0, 0 1 0 0 0, 0 0 1 0 0, 0 0 0 1 0\\\'"
+["values=..0.33330.33330.3333000.33330.33330.3333000.33330.33330.33330000010.."]="values=\\\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\\\'"
+)
+#css static urls
+replacements+=(["url("]="${static}")
+
+
+conditions=$(for key in "${!replacements[@]}"; do echo -n "s/${key}/${replacements[${key}]}/g;"; done)
+
+sed -i "${conditions}" $1
