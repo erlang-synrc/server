16f2adfebb59b780640c3ba37d535db090f912af
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-01-14 14:18:17 +0200

    tournament lobby to public

--- apps/nsp_srv/src/tournament_lobby.erl
+++ apps/nsp_srv/src/tournament_lobby.erl
@@ -20,12 +20,6 @@
 -define(JOINEDPERPAGE, 32). 
 
 main() ->
-    case wf:user() /= undefined of
-        true  -> main_authorized();
-        false -> wf:redirect_to_login(?_U("/login"))
-    end.
-
-main_authorized() ->
     wf:session(this_must_be_unique_joined_page, 1),
     TID = wf:q(id),
     wf:state(tournament_id, TID),
