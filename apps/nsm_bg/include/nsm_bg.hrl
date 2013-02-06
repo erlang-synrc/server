ab11b19e8b0e9f1fb0df0271a06989303f68b57f
Maxim Sokhatsky <maxim@synrc.com>
2013-02-06 14:18:06 +0200

    change bootstrap queue name

--- apps/nsm_bg/include/nsm_bg.hrl
+++ apps/nsm_bg/include/nsm_bg.hrl
@@ -13,7 +13,7 @@
 -define(gv, proplists:get_value).
 
 -define(DEAD_LETTER_EXCHANGE, <<"nsm_bg.dead_letter_exchange.fanout">>).
--define(BOOTSTRAP_WORKER_QUEUE, <<"nsm_bg.worker.bootstrap">>).
+-define(BOOTSTRAP_WORKER_QUEUE, <<"nsm_bg.worker.bootstrap2">>).
 -define(REANIMATOR_QUEUE_NAME(Node),list_to_binary(lists:concat(["nsm_bg.reanimator.", Node]))).
 
 %% this name is user to register in gproc and to create/consume queue.
