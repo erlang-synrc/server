8427a8b66bb709edf551166f4d08340b08806d78
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-02-08 18:38:09 +0200

    fix

--- apps/nsp_srv/src/tournaments.erl
+++ apps/nsp_srv/src/tournaments.erl
@@ -628,7 +628,7 @@ invite_email(Subject, PlainTextTemplate) ->
       Mail = User#user.email,
       Username = User#user.username,
       PlainText = ?_TS(PlainTextTemplate, [{username, Username}]),
-      ?INFO("Mail ~p",[{User#user.email,Subject,PlainText}])
+      ?INFO("Mail ~p",[{User#user.email,Subject,PlainText}]),
       nsx_msg:notify_email(Subject, PlainText, Mail)
    end || User <- Users, User#user.email /= undefined, User#user.status == ok].
 
