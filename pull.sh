6d149eeec426647ecf2f689265f530f8cf568cd2
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-02-11 17:14:19 +0200

    pull before fix

--- pull.sh
+++ pull.sh
@@ -8,10 +8,10 @@ git checkout ktournaments.min.css
 git checkout krules_okey.min.css
 git checkout kgifts.min.css
 
+git pull
+
 ./cssfix.sh kindex.min.css
 ./cssfix.sh kmatchmaker.min.css
 ./cssfix.sh ktournaments.min.css
 ./cssfix.sh krules_okey.min.css
 ./cssfix.sh kgifts.min.css
-
-git pull
