d88db1ec819361a5a16d6840fb0077e808ba33bd
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-02-11 15:34:48 +0200

    pull.sh

---
+++ pull.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd apps/nsp_srv/priv/static/css
+
+git checkout kindex.min.css
+git checkout kmatchmaker.min.css
+git checkout ktournaments.min.css
+git checkout krules_okey.min.css
+git checkout kgifts.min.css
+
+./cssfix.sh kindex.min.css
+./cssfix.sh kmatchmaker.min.css
+./cssfix.sh ktournaments.min.css
+./cssfix.sh krules_okey.min.css
+./cssfix.sh kgifts.min.css
+
+git pull
