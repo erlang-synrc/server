9d14ef6421a2e625fd1adfbc479abc8032903485
Sergei Polkovnikov <serge.polkovnikov@gmail.com>
2013-02-18 14:09:14 +0200

    recrunt: Add new records for the tavla

--- apps/nsg_srv/src/recrunt.erl
+++ apps/nsg_srv/src/recrunt.erl
@@ -118,5 +118,7 @@ fields(tavla_surrender_request) -> record_info(fields, tavla_surrender_request);
 fields(tavla_ack) ->               record_info(fields, tavla_ack);
 %%fields(tavla_board) ->             record_info(fields, tavla_board);
 fields(tavla_accept_timeout) ->    record_info(fields, tavla_accept_timeout);
+fields(tavla_tour_result) ->       record_info(fields, tavla_tour_result);
+fields(tavla_tour_record) ->       record_info(fields, tavla_tour_record);
 
 fields(_) -> record_info(fields,unknown_data).
