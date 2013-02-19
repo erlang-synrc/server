b50341190d30677dc6238f4daf67fef2d1a125f7
Sergei Polkovnikov <serge.polkovnikov@gmail.com>
2013-02-19 11:59:44 +0200

    matchmaker: adaptation for tavla paired games

--- apps/nsp_srv/src/matchmaker.erl
+++ apps/nsp_srv/src/matchmaker.erl
@@ -515,6 +515,7 @@ show_table(Tables) ->
             {ok,WholeTable} ->
               MaxUsers = case GameName of 
                 "tavla" ->
+%%                    ?INFO("tavla tournament type:~p", [WholeTable#game_table.tournament_type]),
                   case WholeTable#game_table.tournament_type of
                     paired -> 10;
                     paired_lobby -> 10;
@@ -531,7 +532,12 @@ show_table(Tables) ->
                 "okey" -> Users
               end,
               GameType = WholeTable#game_table.game_type,
+              GameMode = WholeTable#game_table.game_mode,
               FreeSeatsNum = if GameType == game_okey,
+                                GameState == started,
+                                RobotsReplacementAllowed -> MaxUsers - length([U || U <- RealUsers, U=/=robot]);
+                                GameType == game_tavla,
+                                GameMode == paired,
                                 GameState == started,
                                 RobotsReplacementAllowed -> MaxUsers - length([U || U <- RealUsers, U=/=robot]);
                               true -> MaxUsers - length(RealUsers)
