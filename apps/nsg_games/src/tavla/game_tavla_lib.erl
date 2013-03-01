2f21280d5cd82cc6425750193204279323dcf33f
Sergei Polkovnikov <serge.polkovnikov@gmail.com>
2013-03-01 12:50:15 +0200

    game_tavla_lib: A one more type of boards view

--- apps/nsg_games/src/tavla/game_tavla_lib.erl
+++ apps/nsg_games/src/tavla/game_tavla_lib.erl
@@ -12,7 +12,8 @@
 %%
 -export([board_to_text1/1,
          board_to_text2/1,
-         board_to_text3/1]).
+         board_to_text3/1,
+         board_to_text4/1]).
 
 -define(BLACK, black).
 -define(WHITE, white).
@@ -53,6 +54,20 @@ board_to_text3(Board) ->
     "|" ++ quater(1, 6, Board) ++ "|" ++ quater(7, 12, Board) ++ "|" ++
     quater(13, 18, Board) ++ "|" ++ quater(19, 24, Board) ++ "|" ++
     checker(?WHITE_BAR, Board) ++ "(" ++ checker(?BLACK_OUT, Board) ++ ")"].
+
+board_to_text4(Board) ->
+    Str1 = "|xoxoxo+-+xoxoxo| ",
+    Str4 = "|------|+|------| ",
+    Str7 = "|xoxoxo+-+xoxoxo| ",
+    List1 = [13, 14, 15, 16, 17, 18],
+    List2 = [19, 20 ,21, 22, 23, 24],
+    List3 = [12, 11, 10, 09, 08, 07],
+    List4 = [06, 05 ,04, 03, 02, 01],
+    Str2 = "|" ++ list_to_colors2(List1, Board) ++ "|" ++ list_to_colors2([?BLACK_BAR], Board) ++ "|" ++ list_to_colors2(List2, Board) ++ "|" ++ list_to_colors2([?BLACK_OUT], Board),
+    Str3 = "|" ++ list_to_checkers2(List1, Board) ++ "|" ++ list_to_checkers2([?BLACK_BAR], Board) ++ "|" ++ list_to_checkers2(List2, Board) ++ "|" ++ list_to_checkers2([?BLACK_OUT], Board),
+    Str5 = "|" ++ list_to_checkers2(List3, Board) ++ "|" ++ list_to_checkers2([?WHITE_BAR], Board) ++ "|" ++ list_to_checkers2(List4, Board)++ "|" ++ list_to_checkers2([?WHITE_OUT], Board),
+    Str6 = "|" ++ list_to_colors2(List3, Board) ++ "|" ++ list_to_colors2([?WHITE_BAR], Board) ++ "|" ++ list_to_colors2(List4, Board)++ "|" ++ list_to_colors2([?WHITE_OUT], Board),
+    [Str1, Str2, Str3, Str4, Str5, Str6, Str7].
 
 get_checkers(Pos, Board) ->
     {_, Value} = lists:keyfind(Pos, 1, Board),
