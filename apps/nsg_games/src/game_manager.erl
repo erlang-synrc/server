9f8b00be2e1653f3ee8a4fa43bff0336d830bec3
Sergei Polkovnikov <serge.polkovnikov@gmail.com>
2013-03-22 00:16:09 +0200

    game_manager: clean up

--- apps/nsg_games/src/game_manager.erl
+++ apps/nsg_games/src/game_manager.erl
@@ -16,13 +16,7 @@ destroy_game(Pid,Sup) -> game_sup:stop_game(Sup,Pid).
 
 create_game(GameFSM, Params) ->
     GameId = id_generator:get_id(),
-    {{ok,Pid},_} = create_game_monitor2(GameId, GameFSM, Params, self()),
-    {ok, GameId, Pid}.
-
-create_table(GameFSM, PlayerIds) -> create_table(GameFSM, [], PlayerIds).
-create_table(GameFSM, Params, PlayerIds) ->
-    GameId = id_generator:get_id(),
-    {{ok, Pid},_} = create_game_monitor(GameId, {lobby, GameFSM}, Params, PlayerIds, self()),
+    {ok, Pid} = create_game_monitor(GameId, GameFSM, Params),
     {ok, GameId, Pid}.
 
 get_lucky_pid(Sup) ->
@@ -44,19 +38,11 @@ counter(Game) -> PL = supervisor:count_children(case Game of game_okey -> okey_s
                       game_tavla -> Res;
                       _ -> 0 end.
 
-%start([Module, Args]) -> gen_server:start(?MODULE,[Module,Args],[]).
-%start(Module, Args) -> gen_server:start(?MODULE,[Module,Args],[]).
 start() -> gen_server:start({local, ?MODULE}, ?MODULE, [], []).
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 stop(Ref) -> gen_server:cast(Ref, stop).
 
 init([]) -> {ok,#state{}}.
-%init([Module,Args]) -> 
-%    ?INFO("STARTING MODULE ~p under SUPERVISOR with PARAMS ~p~n",[Module,Args]),
-%    case Module:start(Args) of
-%                         {ok,_,Pid} -> {ok, Pid};
-%                         {ok,Pid} -> {ok, Pid}
-%                        end.
 
 handle_call({get_relay, Topic}, _From, State) -> Res = get_relay_pid(Topic), {reply, Res, State};
 handle_call({game_counter, FSM}, _From, State) ->
@@ -69,11 +55,8 @@ handle_info(Info, State) -> {stop, {unknown_info, Info}, State}.
 terminate(_Reason, _State) -> ok.
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
-game_monitor_module(GameFSM, GameMode) -> case {GameFSM, GameMode} of {game_tavla, paired} -> paired_tavla; _ -> relay end.
-get_requirements(GameFSM,M) -> (game_monitor_module(GameFSM, M)):get_requirements(GameFSM,M).
 game_sup_domain(Module, Params) ->
     case Module of
-        game_okey_ng_trn_elim -> okey_sup;
         game_tavla_ng_trn_paired -> tavla_sup;
         nsg_trn_standalone ->
             case proplists:get_value(game, Params) of
@@ -90,34 +73,12 @@ game_sup_domain(Module, Params) ->
         _ -> game_sup
     end.
 
--spec create_game_monitor(string(), pid(), [any()], [pid()], #state{}) -> {{'ok', pid()} | {'error', any()}, #state{}}.
-create_game_monitor(Topic, {lobby,GameFSM}, Params, Players, State) ->
-    GameMode = proplists:get_value(game_mode, Params, standard),
-    ?INFO("Create Root Game Process (Game Monitor): ~p Mode: ~p Params: ~p",[GameFSM, GameMode,Params]),
-    GameModule = game_monitor_module(GameFSM,GameMode),
-    Sup = game_sup_domain(GameFSM, Params),
-    RelayInit = Sup:start_game(GameModule,[Topic, {lobby,GameFSM}, Params, Players, self()], Topic),
-    ?INFO("RelayInit ~p",[RelayInit]),
-    case RelayInit of 
-        {ok, Srv} ->
-%            Ref = erlang:monitor(process, Srv),
-            {{ok, Srv}, State};
-        {error, Reason} ->
-            {{error, Reason}, State}
-    end.
-
-create_game_monitor2(Topic, GameFSM, Params, State) ->
+create_game_monitor(Topic, GameFSM, Params) ->
     Sup = game_sup_domain(GameFSM, Params),
     ?INFO("Create Root Game Process (Game Monitor2): ~p Params: ~p Sup: ~p",[GameFSM, Params,Sup]),
     RelayInit = Sup:start_game(GameFSM,[Topic,Params],Topic),
     ?INFO("RelayInit ~p",[RelayInit]),
-    case RelayInit of 
-        {ok, Srv} ->
-%            Ref = erlang:monitor(process, Srv),
-            {{ok, Srv}, State};
-        {error, Reason} ->
-            {{error, Reason}, State}
-    end.
+    RelayInit.
 
 get_lucky_table(Game) ->
     Lucky = true,
@@ -149,150 +110,27 @@ get_tournament(TrnId) ->
 %    ?INFO("~w:get_tournament Table = ~p", [?MODULE, Table]),
     Table.
 
-tavla_create_tables(Num) ->
 
-    Users = nsm_auth:imagionary_users2(),
-    L = length(Users),
-
-    TavlaTwoPlayers = [ begin
-
-    T2U1 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-    T2U2 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-
-    game_manager:create_table(game_tavla,
-                         [{table_name,"tavla two players"},
-                          {speed,normal},
-                          {rounds,3},
-                          {default_pr,yes},
-                          {game_mode,standard},
-                          {owner,"maxim"}],
-                         [list_to_binary(T2U1),list_to_binary(T2U2)]) end || X<-lists:seq(2,Num)],
-    [{ok,T2P1,_}|_] = TavlaTwoPlayers,
-    [{ok,T2P2,_}|_] = lists:reverse(TavlaTwoPlayers),
-    ?INFO("Tavla two playrs rooms: ~p",[{T2P1,T2P2}]),
-
-    TavlaRobot = [ begin
-
-    T2U1 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-    T2U2 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-
-    game_manager:create_table(game_tavla,[{table_name,"tavla one player"},
-                          {speed,normal},
-                          {rounds,3},
-                          {default_pr,yes},
-                          {game_mode,standard},
-                          {owner,"maxim"}],[list_to_binary(T2U1),robot]) end ||X<-lists:seq(2,Num div 2)],
-    [{ok,TR1,_}|_] = TavlaRobot,
-    [{ok,TR2,_}|_] = lists:reverse(TavlaRobot),
-    ?INFO("Tavla bot rooms: ~p",[{TR1,TR2}]).
-
-
-okey_create_tables(Num) ->
-    PR = #pointing_rule{quota = 1,
-                        kakush_winner = 1,
-                        kakush_other = 1,
-                        game_points = 1
-                       },
-    Params = [{speed,fast},
-              {rounds,3},
-              {double_points, 1},
-              {pointing_rules, PR},
-              {game_mode,color},
-              {speed, normal},
-              {slang, false},
-              {observers, false},
-              {owner,"maxim"}],
-
-    OkeyBots = [begin
-    game_manager:create_standalone_game(game_okey,[{table_name,"okey one player"} | Params],
-                                        [robot,robot,robot]) end || _ <-lists:seq(2,Num)],
-    [{ok,OB1,_}|_] = OkeyBots,
-    [{ok,OB2,_}|_] = lists:reverse(OkeyBots),
-    ?INFO("Okey bot rooms: ~p~n",[{OB1,OB2}]),
-
-    OkeyPlayers = [begin
-    game_manager:create_standalone_game(game_okey,[{table_name,"okey four players"} | Params],
-                                        []) end || _ <-lists:seq(2,Num)],
-    [{ok,OP1,_}|_] = OkeyPlayers,
-    [{ok,OP2,_}|_] = lists:reverse(OkeyPlayers),
-    ?INFO("Okey four players bot rooms: ~p~n",[{OP1,OP2}]).
-
-create_paired(Num) ->
-
-    Users = nsm_auth:imagionary_users2(),
-
-    L = length(Users),
-
-    TavlaPairedPlayers = [begin
-
-    T2U1 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-    T2U2 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-
-    game_manager:create_table(game_tavla,[{table_name,"paired tavla two tables"},
-                          {speed, normal},
-                          {default_pr,yes},
-                          {rounds,3},
-                          {game_mode, paired},
-                          {owner,"maxim"}],[list_to_binary(T2U1),robot, robot, robot]) end || X <-lists:seq(2,Num)],
-    [{ok,TP1,_}|_] = TavlaPairedPlayers,
-    [{ok,TP2,_}|_] = lists:reverse(TavlaPairedPlayers),
-    ?INFO("Paired Tavla 2 tables rooms: ~p",[{TP1,TP2}]),
-
-
-    TavlaPairedPlayers5Tables = [begin
-
-    T2U1 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-    T2U2 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-    T2U3 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-    T2U4 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-    T2U5 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-    T2U6 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-    T2U7 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-    T2U8 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-    T2U9 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-    T2U10 = nsm_auth:ima_gio2(crypto:rand_uniform(0,L),Users),
-
-    game_manager:create_table(game_tavla,[{table_name,"paired tavla 5 tables"},
-                          {speed, normal},
-                          {rounds,3},
-                          {default_pr,yes},
-                          {game_mode, paired},
-                          {owner,"maxim"}],[list_to_binary(T2U1),
-                                            list_to_binary(T2U2),
-                                            list_to_binary(T2U3),
-                                            list_to_binary(T2U4),
-                                            list_to_binary(T2U5),
-                                            list_to_binary(T2U6),
-                                            list_to_binary(T2U7),
-                                            list_to_binary(T2U8),
-                                            list_to_binary(T2U9),
-                                            list_to_binary(T2U10)]) end || X <-lists:seq(2,Num)],
-
-    [{ok,TP15,_}|_] = TavlaPairedPlayers5Tables,
-    [{ok,TP25,_}|_] = lists:reverse(TavlaPairedPlayers5Tables),
-    ?INFO("Paired Tavla 5 tables rooms: ~p",[{TP15,TP25}]),
-    ok.
-
-stress_test(NumberOfRooms) ->
-    OkeyPlayers = [begin
-          {ok,GameId,A} = game_manager:create_table(game_okey,[{table_name,"okey maxim and alice + 2 robots"},
-                          {speed,normal},
-                          {rounds,80},
-                          {sets,1},
-                          {game_mode,standard},
-                          {owner,"kate"}],[<<"maxim">>,<<"alice">>,robot,robot]),
-
-            Clients = [ proc_lib:spawn_link(fun() -> 
-                                 test_okey:init_with_join_game(self(), '127.0.0.1', ?LISTEN_PORT, GameId, Id, 1, normal)
-                        end) || Id <- [<<"maxim">>,<<"alice">>] ],
-
-                    {ok,GameId,A}
-                  
-
-                   end ||X<-lists:seq(1,NumberOfRooms)],
-    [{ok,OP1,_}|_] = OkeyPlayers,
-    [{ok,OP2,_}|_] = lists:reverse(OkeyPlayers),
-    ?INFO("Okey bot rooms runned (STRESS): ~p~n",[{OP1,OP2}]).
+%% stress_test(NumberOfRooms) ->
+%%     OkeyPlayers = [begin
+%%           {ok,GameId,A} = game_manager:create_table(game_okey,[{table_name,"okey maxim and alice + 2 robots"},
+%%                           {speed,normal},
+%%                           {rounds,80},
+%%                           {sets,1},
+%%                           {game_mode,standard},
+%%                           {owner,"kate"}],[<<"maxim">>,<<"alice">>,robot,robot]),
+%% 
+%%             Clients = [ proc_lib:spawn_link(fun() -> 
+%%                                  test_okey:init_with_join_game(self(), '127.0.0.1', ?LISTEN_PORT, GameId, Id, 1, normal)
+%%                         end) || Id <- [<<"maxim">>,<<"alice">>] ],
+%% 
+%%                     {ok,GameId,A}
+%%                   
+%% 
+%%                    end ||X<-lists:seq(1,NumberOfRooms)],
+%%     [{ok,OP1,_}|_] = OkeyPlayers,
+%%     [{ok,OP2,_}|_] = lists:reverse(OkeyPlayers),
+%%     ?INFO("Okey bot rooms runned (STRESS): ~p~n",[{OP1,OP2}]).
 
 
 create_standalone_game(Game, Params, Users) ->
@@ -413,7 +251,6 @@ create_standalone_game(Game, Params, Users) ->
                           {table_params, TableParams},
                           {common_params, Params}
                          ])
-%%            create_table(Game, Params, Users)
     end.
 
 
@@ -447,9 +284,7 @@ create_paired_game(Game, Params, Users) ->
                            {observers_allowed, ObserversAllowed},
                            {tournament_type, paired},
                            {round_timeout, infinity},
-%%                           {round_timeout, 30 * 1000},
                            {set_timeout, infinity},
-%%                           {set_timeout, 10 * 60 *1000},
                            {speed, Speed},
                            {game_mode, GameMode},
                            {rounds, Rounds},
@@ -608,19 +443,7 @@ start_tournament(TrnId,NumberOfTournaments,NumberOfPlayers,_Quota,_Tours,_Speed,
                        {speed, Speed},
                        {awards, GiftIds}],
              {ok,GameId,A} = create_elimination_trn(GameType, Params, Registrants),
-
-%%              {ok,GameId,A} = game_manager:create_game(game_okey_ng_trn_elim, [{registrants, Registrants},
-%%                                                                {quota_per_round, Tournament#tournament.quota},
-%%                                                                {players, NumberOfPlayers},
-%%                                                                {tours, Tournament#tournament.tours},
-%%                                                                {speed, Tournament#tournament.speed},
-%%                                                                {game_mode, Tournament#tournament.game_mode},
-%%                                                                {awards, GiftIds},
-%%                                                                {trn_id,TrnId},
-%%                                                                {demo_mode, false}]),
-
              nsm_db:put(Tournament#tournament{status=activated}),
-
              {ok,GameId,A}
          end || _ <-lists:seq(1,NumberOfTournaments)],
     [{ok,OP1,_}|_] = OkeyTournaments,
